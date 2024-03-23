46bca46cfa994bb49e4afab5fe5c36464f94a897
RyanGormican <ryan.gormican@gmail.com>
2024-03-23 02:18:41 -0600

    a
    a

--- src/components/Energize.js
+++ src/components/Energize.js
@@ -1,25 +1,27 @@
-import React, {useState} from 'react';
-import { MotivationPerSecond} from './Calculations';
-const Energize = ({ stats, setStats }) => {
-const [view,setView] = useState('Items');
+import React, { useState } from 'react';
+import {Tooltip} from 'react-tooltip';
+import { MotivationPerSecond } from './Calculations';
 
-const buyUpgrade = (upgrade) => {
-  const currentPrice = upgrade.price;
-  if (stats.motivation >= currentPrice && !upgrade.purchased) {
-    setStats(prevStats => ({
-      ...prevStats,
-      motivation: prevStats.motivation - currentPrice,
-      upgrades: prevStats.upgrades.map(item => {
-        if (item.name === upgrade.name) {
-          return { ...item, purchased: true };
-        }
-        return item;
-      })
-    }));
-  } else {
-    alert("Not enough motivation!");
-  }
-};
+const Energize = ({ stats, setStats }) => {
+  const [view, setView] = useState('Items');
+
+  const buyUpgrade = (upgrade) => {
+    const currentPrice = upgrade.price;
+    if (stats.motivation >= currentPrice && !upgrade.purchased) {
+      setStats(prevStats => ({
+        ...prevStats,
+        motivation: prevStats.motivation - currentPrice,
+        upgrades: prevStats.upgrades.map(item => {
+          if (item.name === upgrade.name) {
+            return { ...item, purchased: true };
+          }
+          return item;
+        })
+      }));
+    } else {
+      alert("Not enough motivation!");
+    }
+  };
 
   const buyItem = (itemType) => {
     const currentPrice = calculatePrice(itemType);
@@ -33,21 +35,25 @@ const buyUpgrade = (upgrade) => {
       alert("Not enough motivation to buy!");
     }
   };
+  const totalFoamFingerEffect = stats.upgrades
+  .filter(upgrade => upgrade.area === 'foamfinger' && upgrade.purchased)
+  .reduce((total, upgrade) => total + upgrade.effect, 0);
 
+ 
   const hypeUp = () => {
     setStats(prevStats => ({
       ...prevStats,
-      motivation: prevStats.motivation + ( ( 1 + 1* stats.foamfinger) * stats.achievementMultiplier ) ,
+      motivation: prevStats.motivation + ((1 + (1+totalFoamFingerEffect) * stats.foamfinger ) * stats.achievementMultiplier),
     }));
   };
 
   const calculatePrice = (itemType) => {
     let basePrice, increaseRate;
-    switch(itemType) {
+    switch (itemType) {
       case 'foamfinger':
-      basePrice=50;
-      increaseRate = 1.01;
-      break;
+        basePrice = 50;
+        increaseRate = 1.01;
+        break;
       case 'motivationPoster':
         basePrice = 30;
         increaseRate = 1.07;
@@ -85,83 +91,89 @@ const buyUpgrade = (upgrade) => {
       return basePrice;
     }
   };
+
   const purchasedUpgradesCount = stats.upgrades.filter(upgrade => upgrade.purchased).length;
   const totalUpgradesCount = stats.upgrades.length;
+
   return (
     <div className="resources">
-      <p>Motivation: {stats.motivation.toFixed(2)}   ({MotivationPerSecond(stats).toFixed(2)} /sec)<button onClick={hypeUp}>Hype up</button></p>
-     <button onClick={()=> setView('Items') }> Items </button>      <button onClick={()=> setView('Upgrades') }> Upgrades {purchasedUpgradesCount} /  {totalUpgradesCount}  </button>
+      <p>Motivation: {stats.motivation.toFixed(2)} ({MotivationPerSecond(stats).toFixed(2)} /sec)<button onClick={hypeUp}>Hype up</button></p>
+      <button onClick={() => setView('Items')}>Items</button> <button onClick={() => setView('Upgrades')}>Upgrades {purchasedUpgradesCount} / {totalUpgradesCount}</button>
       {view === 'Items' && (
-      <div>
-       <div>
-        <p>
-          Foam Finger (+{(stats.foamfinger * 1 * stats.achievementMultiplier ).toFixed(2)} /click):
-          {stats.foamfinger} --- {calculatePrice('foamfinger')}
-          <button onClick={() => buyItem('foamfinger')}>Buy Foam Finger</button>
-        </p>
-      </div>
-     <div>
-        <p>
-          Motivation Poster (+{(stats.motivationPoster * 1 * stats.achievementMultiplier).toFixed(2)}/sec):
-          {stats.motivationPoster} --- {calculatePrice('motivationPoster')}
-          <button onClick={() => buyItem('motivationPoster')}>Buy Motivation Poster</button>
-        </p>
-      </div>
-      <div>
-        <p>
-          Self Help Book (+{(stats.selfHelpBook * 10 * stats.achievementMultiplier).toFixed(2)}/sec):
-          {stats.selfHelpBook} --- {calculatePrice('selfHelpBook')}
-          <button onClick={() => buyItem('selfHelpBook')}>Buy Self Help Book</button>
-        </p>
-      </div>
-      <div>
-        <p>
-          Meditation Guide (+{(stats.meditationGuide * 20 * stats.achievementMultiplier).toFixed(2)}/sec):
-          {stats.meditationGuide} --- {calculatePrice('meditationGuide')}
-          <button onClick={() => buyItem('meditationGuide')}>Buy Meditation Guide</button>
-        </p>
-      </div>
-      <div>
-        <p>
-          Yoga Mat (+{(stats.yogaMat * 25 * stats.achievementMultiplier).toFixed(2)}/sec):
-          {stats.yogaMat} --- {calculatePrice('yogaMat')}
-          <button onClick={() => buyItem('yogaMat')}>Buy Yoga Mat</button>
-        </p>
-      </div>
-      <div>
-        <p>
-          Energy Drink (+{(stats.energyDrink * 30 * stats.achievementMultiplier).toFixed(2)}/sec):
-          {stats.energyDrink} --- {calculatePrice('energyDrink')}
-          <button onClick={() => buyItem('energyDrink')}>Buy Energy Drink</button>
-        </p>
-      </div>
-      <div>
-        <p>
-          Influencer Course (+{(stats.influencerCourse * 50 * stats.achievementMultiplier).toFixed(2)}/sec):
-          {stats.influencerCourse} --- {calculatePrice('influencerCourse')}
-          <button onClick={() => buyItem('influencerCourse')}>Buy Influencer Course</button>
-        </p>
-      </div>
-      </div>
+        <div>
+          <div>
+            <p>
+              Foam Finger (+{(stats.foamfinger *( 1 + totalFoamFingerEffect)* stats.achievementMultiplier).toFixed(2)} /click):
+              {stats.foamfinger} --- {calculatePrice('foamfinger')}
+              <button onClick={() => buyItem('foamfinger')}>Buy Foam Finger</button>
+            </p>
+          </div>
+                  <div>
+            <p>
+              Motivation Poster (+{(stats.motivationPoster * 1 * stats.achievementMultiplier).toFixed(2)}/sec):
+              {stats.motivationPoster} --- {calculatePrice('motivationPoster')}
+              <button onClick={() => buyItem('motivationPoster')}>Buy Motivation Poster</button>
+            </p>
+          </div>
+          <div>
+            <p>
+              Self Help Book (+{(stats.selfHelpBook * 10 * stats.achievementMultiplier).toFixed(2)}/sec):
+              {stats.selfHelpBook} --- {calculatePrice('selfHelpBook')}
+              <button onClick={() => buyItem('selfHelpBook')}>Buy Self Help Book</button>
+            </p>
+          </div>
+          <div>
+            <p>
+              Meditation Guide (+{(stats.meditationGuide * 20 * stats.achievementMultiplier).toFixed(2)}/sec):
+              {stats.meditationGuide} --- {calculatePrice('meditationGuide')}
+              <button onClick={() => buyItem('meditationGuide')}>Buy Meditation Guide</button>
+            </p>
+          </div>
+          <div>
+            <p>
+              Yoga Mat (+{(stats.yogaMat * 25 * stats.achievementMultiplier).toFixed(2)}/sec):
+              {stats.yogaMat} --- {calculatePrice('yogaMat')}
+              <button onClick={() => buyItem('yogaMat')}>Buy Yoga Mat</button>
+            </p>
+          </div>
+          <div>
+            <p>
+              Energy Drink (+{(stats.energyDrink * 30 * stats.achievementMultiplier).toFixed(2)}/sec):
+              {stats.energyDrink} --- {calculatePrice('energyDrink')}
+              <button onClick={() => buyItem('energyDrink')}>Buy Energy Drink</button>
+            </p>
+          </div>
+          <div>
+            <p>
+              Influencer Course (+{(stats.influencerCourse * 50 * stats.achievementMultiplier).toFixed(2)}/sec):
+              {stats.influencerCourse} --- {calculatePrice('influencerCourse')}
+              <button onClick={() => buyItem('influencerCourse')}>Buy Influencer Course</button>
+            </p>
+          </div>
+        </div>
       )}
       {view === 'Upgrades' && (
         <div>
+    
           {stats?.upgrades
             .filter(upgrade => upgrade.type === 'motivation')
             .map((upgrade, index) => (
-              <div key={index}>
-                <p>
+              <div key={index} className="upgrade-container">
+                <p className="upgrade-info">
                   {upgrade.name}: {upgrade.price}
                   <button disabled={upgrade.purchased} onClick={() => buyUpgrade(upgrade)}>
                     {upgrade.purchased ? "Purchased" : "Purchase"}
                   </button>
+                  <span className="tooltip-text">{upgrade.description}</span>
                 </p>
               </div>
             ))}
         </div>
       )}
+
     </div>
   );
 };
 
 export default Energize;
+
