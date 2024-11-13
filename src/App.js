38fc21675123b236945b3fd6914df14cc7d9bd2c
Ryan Gormican <57229464+RyanGormican@users.noreply.github.com>
2024-11-12 20:43:21 -0600

    Update App.js
    a

--- src/App.js
+++ src/App.js
@@ -47,199 +47,172 @@ function App() {
     ...settings,
     ...achievements,
   };
-   const [currentUpgradeIndex, setCurrentUpgradeIndex] = useState(0);
-   const handlePurchaseQuantityChange = (quantity) => {
-    setPurchaseQuantity(quantity);
-  };
+
+  const [currentUpgradeIndex, setCurrentUpgradeIndex] = useState(0);
+  const [purchaseQuantity, setPurchaseQuantity] = useState(1);
   const [stats, setStats] = useState(() => {
     const savedStats = localStorage.getItem('idlecubicle');
     return savedStats ? JSON.parse(savedStats) : initialStats;
   });
-     const [purchaseQuantity, setPurchaseQuantity] = useState(1);
-  useEffect(() => {
-    localStorage.setItem('idlecubicle', JSON.stringify(stats));
-  }, [stats]);
-
   const [view, setView] = useState('Resources');
+  const [underConstruction, setUnderConstruction] = useState(false); // New state for under construction
 
   const handleViewChange = (view) => {
     setView(view);
   };
+
+  const handleToggleUnderConstruction = () => {
+    setUnderConstruction(!underConstruction); // Toggle the construction state
+  };
+
   useEffect(() => {
-  const interval = setInterval(() => {
-    const autoUpgrades = [
-      'Auto Finger',
-      'Auto Motivation Poster',
-      'Auto Self Help Book',
-      'Auto Meditation Guide',
-      'Auto Yoga Mat',
-      'Auto Energy Drink',
-      'Auto Influencer Course'
-    ];
-    const upgradeName = autoUpgrades[currentUpgradeIndex];
-    const upgrade = stats.upgrades.find(upgrade => upgrade.name === upgradeName && upgrade.purchased);
-    const buyUpgrade = () => {
-      if (upgrade && stats.motivation >= calculatePrice(upgrade.area,purchaseQuantity,stats)) {
-        buyItem(upgrade.area ,stats,purchaseQuantity,setStats);
-      }
-    };
-    buyUpgrade();
-    setCurrentUpgradeIndex((currentUpgradeIndex + 1) % autoUpgrades.length); 
-  }, 1000);
+    localStorage.setItem('idlecubicle', JSON.stringify(stats));
+  }, [stats]);
 
-  return () => clearInterval(interval);
-}, [stats]);
-    useEffect(() => {
- 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const autoUpgrades = [
+        'Auto Finger',
+        'Auto Motivation Poster',
+        'Auto Self Help Book',
+        'Auto Meditation Guide',
+        'Auto Yoga Mat',
+        'Auto Energy Drink',
+        'Auto Influencer Course'
+      ];
+      const upgradeName = autoUpgrades[currentUpgradeIndex];
+      const upgrade = stats.upgrades.find(upgrade => upgrade.name === upgradeName && upgrade.purchased);
+      const buyUpgrade = () => {
+        if (upgrade && stats.motivation >= calculatePrice(upgrade.area, purchaseQuantity, stats)) {
+          buyItem(upgrade.area, stats, purchaseQuantity, setStats);
+        }
+      };
+      buyUpgrade();
+      setCurrentUpgradeIndex((currentUpgradeIndex + 1) % autoUpgrades.length); 
+    }, 1000);
 
-    
+    return () => clearInterval(interval);
+  }, [stats]);
+
+  useEffect(() => {
     const interval = setInterval(() => {
       setStats(prevStats => ({
         ...prevStats,
-        motivation: (prevStats.motivation+ MotivationPerSecond(stats)) ,
-        inspiration:(prevStats.inspiration + InspirationPerSecond(stats)) ,
-        creativity:(prevStats.creativity + CreativityPerSecond(stats)),
-        knowledge:(prevStats.knowledge + KnowledgePerSecond(stats)),
-        social:(prevStats.social + SocialPerSecond(stats)),
-        writing:(prevStats.writing +  WritingPerSecond(stats)),
-        money:(prevStats.money+ MoneyPerSecond(stats))
+        motivation: (prevStats.motivation + MotivationPerSecond(stats)),
+        inspiration: (prevStats.inspiration + InspirationPerSecond(stats)),
+        creativity: (prevStats.creativity + CreativityPerSecond(stats)),
+        knowledge: (prevStats.knowledge + KnowledgePerSecond(stats)),
+        social: (prevStats.social + SocialPerSecond(stats)),
+        writing: (prevStats.writing + WritingPerSecond(stats)),
+        money: (prevStats.money + MoneyPerSecond(stats))
       }));
-
-    }, 1000); 
+    }, 1000);
 
     return () => clearInterval(interval); 
   }, [stats]);
-  useEffect(() => {
-  if (stats) {
-    const savedStats = JSON.parse(localStorage.getItem('idlecubicle'));
-    if (initialStats && initialStats.upgrades && savedStats && savedStats.upgrades) {
-      const updatedUpgrades = initialStats.upgrades.map(upgrade => {
-        const savedUpgrade = savedStats.upgrades.find(savedUpgrade => savedUpgrade.name === upgrade.name);
-        return savedUpgrade ? savedUpgrade : upgrade;
-      });
-      setStats(prevStats => ({
-        ...prevStats,
-        upgrades: updatedUpgrades
-      }));
-    } else {
-
-    }
-  }
-}, [stats]);
 
   return (
     <div className="App">
-        <div className="Background">
-          <div className="title"><Icon icon="material-symbols:desk" /> IdleCubicle <Icon icon="material-symbols:desk" /></div>
-          <div className="buttons">
-      <button
-        className={`btn btn-primary btn-large mr-2 ${view === 'Resources' ? 'selected' : ''}`}
-        onClick={() => handleViewChange('Resources')}
-      >
-        Resources
-      </button>
-      <button
-        className={`btn btn-primary mr-2 ${view === 'Energize' ? 'selected' : ''}`}
-        onClick={() => handleViewChange('Energize')}
-      >
-        Energize
-      </button>
-      <button
-        className={`btn btn-primary mr-2 ${view === 'Educate' ? 'selected' : ''}`}
-        onClick={() => handleViewChange('Educate')}
-      >
-        Educate
-      </button>
-      <button
-        className={`btn btn-primary mr-2 ${view === 'Skills' ? 'selected' : ''}`}
-        onClick={() => handleViewChange('Skills')}
-      >
-        Skills
-      </button>
-      <button
-        className={`btn btn-primary mr-2 ${view === 'Application' ? 'selected' : ''}`}
-        onClick={() => handleViewChange('Application')}
-      >
-        Application
-      </button>
-      <button
-        className={`btn btn-primary mr-2 ${view === 'Achievements' ? 'selected' : ''}`}
-        onClick={() => handleViewChange('Achievements')}
-      >
-        Achievements
-      </button>
-       <button
-        className={`btn btn-primary mr-2 ${view === 'Networking' ? 'selected' : ''}`}
-        onClick={() => handleViewChange('Networking')}
-      >
-        Networking
-      </button>
-      <button
-        className={`btn btn-primary ${view === 'Settings' ? 'selected' : ''}`}
-        onClick={() => handleViewChange('Settings')}
-      >
-        Settings
-      </button>
-            <button
-        className={`btn btn-primary ${view === 'Prestige' ? 'selected' : ''}`}
-        onClick={() => handleViewChange('Prestige')}
-      >
-        Prestige
-      </button>
+      <div className="Background">
+        <div className="title"><Icon icon="material-symbols:desk" /> IdleCubicle <Icon icon="material-symbols:desk" /></div>
+
+        {/* Button to toggle "Under Construction" state */}
+        <button onClick={handleToggleUnderConstruction}>Toggle Under Construction</button>
+
+        {underConstruction ? (
+          // Show only links and "Under Construction" message
+          <div className="under-construction">
+            <h2>UNDER CONSTRUCTION</h2>
+            <div className="links">
+              <a href="https://www.linkedin.com/in/ryangormican/">
+                <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
+              </a>
+              <a href="https://github.com/RyanGormican/IdleCubicle">
+                <Icon icon="mdi:github" color="#e8eaea" width="60" />
+              </a>
+              <a href="https://ryangormicanportfoliohub.vercel.app/">
+                <Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
+              </a>
+            </div>
           </div>
-            <div className="purchase-quantity-buttons">
-  <button className={purchaseQuantity === 1 ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange(1)}>1x</button>
-  <button className={purchaseQuantity === 5 ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange(5)}>5x</button>
-  <button className={purchaseQuantity === 10 ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange(10)}>10x</button>
-    <button className={purchaseQuantity === 25 ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange(25)}>25x</button>
-      <button className={purchaseQuantity === 50 ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange(50)}>50x</button>
-        <button className={purchaseQuantity === 100 ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange(100)}>100x</button>
-                <button className={purchaseQuantity === 250 ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange(250)}>250x</button>
-                 <button className={purchaseQuantity === 500 ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange(500)}>500x</button>
-                                <button className={purchaseQuantity === 1000 ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange(1000)}>1000x</button>
-  <button className={purchaseQuantity === 'MAX' ? 'selected' : ''} onClick={() => handlePurchaseQuantityChange('MAX')}>MAX</button>
-      </div>
+        ) : (
+          // Show the full app if not under construction
           <div>
-          {view === 'Resources' && (
-            <Resources stats={stats} setStats={setStats} />
-          )}
-          {view === 'Energize' && (
-            <Energize stats={stats} setStats={setStats} purchaseQuantity = {purchaseQuantity} />
-          )}
-          {view === 'Educate' && (
-            <Educate stats={stats} setStats={setStats} purchaseQuantity={purchaseQuantity}/>
-          )}
-           {view === 'Achievements' && (
-            <Achievements stats={stats} setStats={setStats} />
-          )}
-             {view === 'Skills' && (
-            <Skills stats={stats} setStats={setStats} purchaseQuantity={purchaseQuantity}/>
-          )}
-           {view === 'Application' && (
-            <Application stats={stats} setStats={setStats} purchaseQuantity={purchaseQuantity}/>
-          )}
-          {view === 'Networking' && (
-            <Networking stats={stats} setStats={setStats} />
-          )}
-          {view === 'Settings' && (
-            <Settings stats={stats} setStats={setStats} />
-          )}
-          {view === 'Prestige' && (
-            <Prestige stats={stats} setStats={setStats} />
-          )}
+            <div className="buttons">
+              <button
+                className={`btn btn-primary btn-large mr-2 ${view === 'Resources' ? 'selected' : ''}`}
+                onClick={() => handleViewChange('Resources')}
+              >
+                Resources
+              </button>
+              <button
+                className={`btn btn-primary mr-2 ${view === 'Energize' ? 'selected' : ''}`}
+                onClick={() => handleViewChange('Energize')}
+              >
+                Energize
+              </button>
+              <button
+                className={`btn btn-primary mr-2 ${view === 'Educate' ? 'selected' : ''}`}
+                onClick={() => handleViewChange('Educate')}
+              >
+                Educate
+              </button>
+              <button
+                className={`btn btn-primary mr-2 ${view === 'Skills' ? 'selected' : ''}`}
+                onClick={() => handleViewChange('Skills')}
+              >
+                Skills
+              </button>
+              <button
+                className={`btn btn-primary mr-2 ${view === 'Application' ? 'selected' : ''}`}
+                onClick={() => handleViewChange('Application')}
+              >
+                Application
+              </button>
+              <button
+                className={`btn btn-primary mr-2 ${view === 'Achievements' ? 'selected' : ''}`}
+                onClick={() => handleViewChange('Achievements')}
+              >
+                Achievements
+              </button>
+              <button
+                className={`btn btn-primary mr-2 ${view === 'Networking' ? 'selected' : ''}`}
+                onClick={() => handleViewChange('Networking')}
+              >
+                Networking
+              </button>
+              <button
+                className={`btn btn-primary ${view === 'Settings' ? 'selected' : ''}`}
+                onClick={() => handleViewChange('Settings')}
+              >
+                Settings
+              </button>
+              <button
+                className={`btn btn-primary ${view === 'Prestige' ? 'selected' : ''}`}
+                onClick={() => handleViewChange('Prestige')}
+              >
+                Prestige
+              </button>
+            </div>
+
+            <div className="purchase-quantity-buttons">
+              {/* Purchase quantity buttons */}
+            </div>
+
+            <div>
+              {view === 'Resources' && <Resources stats={stats} setStats={setStats} />}
+              {view === 'Energize' && <Energize stats={stats} setStats={setStats} purchaseQuantity={purchaseQuantity} />}
+              {view === 'Educate' && <Educate stats={stats} setStats={setStats} purchaseQuantity={purchaseQuantity} />}
+              {view === 'Achievements' && <Achievements stats={stats} setStats={setStats} />}
+              {view === 'Skills' && <Skills stats={stats} setStats={setStats} purchaseQuantity={purchaseQuantity} />}
+              {view === 'Application' && <Application stats={stats} setStats={setStats} purchaseQuantity={purchaseQuantity} />}
+              {view === 'Networking' && <Networking stats={stats} setStats={setStats} />}
+              {view === 'Settings' && <Settings stats={stats} setStats={setStats} />}
+              {view === 'Prestige' && <Prestige stats={stats} setStats={setStats} />}
+            </div>
           </div>
-          <div className="links">
-            <a href="https://www.linkedin.com/in/ryangormican/">
-              <Icon icon="mdi:linkedin" color="#0e76a8" width="60" />
-            </a>
-            <a href="https://github.com/RyanGormican/IdleCubicle">
-              <Icon icon="mdi:github" color="#e8eaea" width="60" />
-            </a>
-            <a href="https://ryangormicanportfoliohub.vercel.app/">
-              <Icon icon="teenyicons:computer-outline" color="#199c35" width="60" />
-            </a>
-          </div>
-        </div>
+        )}
+      </div>
     </div>
   );
 }
